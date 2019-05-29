182dbeb66d89e7a46ff03e397dc99d3b363bfb79
lex_node <gabriel.j.shapiro@gmail.com>
2019-05-28 21:28:37 -0400

    adds code examples for principles 1 & 2 of this.js

--- assignments/this.js
+++ assignments/this.js
@@ -4,7 +4,7 @@
 * 1. When "this" is globally scoped, "this" refers to the uber-Object, the form of forms, the great prototype of prototypes
 * 2. When "this" is used within the method of an object, and then that method is called on that object, "this" refers to that object.
 * 3. When "this" is used within a constructor, calling that constructor makes "this" refer to the specific instance of the class that is created by that call.
-* 4. Using "call" or "apply" together with a parameter can override  "this"'s default application to a constructed instance in favor of applying to a different constructed instance. 
+* 4. Using "call" or "apply" together with a parameter can override  "this"'s default application to a constructed instance in favor of applying to a different constructed instance.
 *
 * write out a code example of each explanation above
 */
@@ -12,10 +12,22 @@
 // Principle 1
 
 // code example for Window Binding
-
+const thisLogger = function(){
+console.log(this);
+}
+thisLogger();
 // Principle 2
 
 // code example for Implicit Binding
+const impliciturBinder = {
+    favoriteImplicationForm: "insinuation",
+    findFave: function() {
+        console.log(`${this.favoriteImplicationForm} is the best form of implication`);
+    }
+}
+
+impliciturBinder.findFave();
+
 
 // Principle 3
 
