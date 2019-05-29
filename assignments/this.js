55d5a5970f85ac02271062ec1be12725e3b8be25
lex_node <gabriel.j.shapiro@gmail.com>
2019-05-28 21:17:05 -0400

    updates README progress; provides explanatory answers of 'this' princples in this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. When "this" is globally scoped, "this" refers to the uber-Object, the form of forms, the great prototype of prototypes
+* 2. When "this" is used within the method of an object, and then that method is called on that object, "this" refers to that object.
+* 3. When "this" is used within a constructor, calling that constructor makes "this" refer to the specific instance of the class that is created by that call.
+* 4. Using "call" or "apply" together with a parameter can override  "this"'s default application to a constructed instance in favor of applying to a different constructed instance. 
 *
 * write out a code example of each explanation above
 */
